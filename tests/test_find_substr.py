604c7d74d08c210b2d4b24593fce98e33fc3d819
Ари <arianna2811@yandex.ru>
2025-07-15 11:21:14 +0300

    feat: positive two entry test

--- tests/test_find_substr.py
+++ tests/test_find_substr.py
@@ -8,3 +8,11 @@ def positive_one_entry():
 
     result = hw1.find_substr(text, pattern)
     assert result == expected, f"expected: {expected}, result: {result}."
+
+def positive_two_entry():
+    text = "мне лень придумывать что писать, лень"
+    pattern = "лень"
+    expected = 4
+
+    result = hw1.find_substr(text, pattern)
+    assert result == expected, f"expected: {expected}, result: {result}."
