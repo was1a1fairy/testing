4796cc96f7e962d8c842de6b17b3023ba21568dd
Ари <arianna2811@yandex.ru>
2025-07-15 15:36:43 +0300

    feat: + test for count func

--- tests/test_count_occurrences.py
+++ tests/test_count_occurrences.py
@@ -25,9 +25,17 @@ def test_positive_empty():
 
     assert result == expected, f"expected:{expected}, result: {result}"
 
-def test_negative_zero_entry():
+def test_negative_not_list():
     lst = "string"
     value = "t"
 
     with pytest.raises(TypeError):
         hw1.count_occurrences(lst, value)
+
+def test_positive_diff_types():
+    lst = [1, "2", 4.44, -5]
+    value = 1
+    expected = 1
+    result = hw1.count_occurrences(lst, value)
+
+    assert result == expected, f"expected:{expected}, result: {result}"
