485e0c79f1f89472e525ee00cad49f440ec310af
Ари <arianna2811@yandex.ru>
2025-07-15 13:23:15 +0300

    feat: + test for find_elem func

--- tests/test_find_elem.py
+++ tests/test_find_elem.py
@@ -17,3 +17,12 @@ def test_positive_two_entry():
 
     result = hw1.find_elem(lst, value)
     assert result == expected, f"expected: {expected}, result: {result}"
+
+
+def test_positive_diff_types():
+    lst = ["мяу", 2, -5, 4.33]
+    value = -5
+    expected = 2
+
+    result = hw1.find_elem(lst, value)
+    assert result == expected, f"expected: {expected}, result: {result}"
