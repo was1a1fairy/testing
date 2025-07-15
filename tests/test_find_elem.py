30b19f93a6e3040c5b83ca7cce0fcff7c6a983e2
Ари <arianna2811@yandex.ru>
2025-07-15 13:19:59 +0300

    feat: + test for find_elem func

--- tests/test_find_elem.py
+++ tests/test_find_elem.py
@@ -8,3 +8,12 @@ def test_positive_one_entry():
 
     result = hw1.find_elem(lst, value)
     assert result == expected, f"expected: {expected}, result: {result}"
+
+
+def test_positive_two_entry():
+    lst = [1, 3, 3, 4]
+    value = 3
+    expected = 1
+
+    result = hw1.find_elem(lst, value)
+    assert result == expected, f"expected: {expected}, result: {result}"
