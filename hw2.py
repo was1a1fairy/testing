1cbd71f3e1768a1ecbefc1178af1e6b2f51b152f
Ари <arianna2811@yandex.ru>
2025-07-20 18:52:34 +0300

    feat: add lenth and add func

--- hw2.py
+++ hw2.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 class Vector:
 
     def __init__(self, x, y, z):
@@ -13,3 +14,9 @@ class Vector:
 
     def get_z(self):
         return self.__z
+
+    def lenth(self):
+        return 0.5**((self.__x**2)+(self.__y**2)+(self.__z**2))
+
+    def add(self, other: Vector):
+        return Vector(self.__x + other.get_x, self.__y + other.get_y, self.__z + other.get_z)
