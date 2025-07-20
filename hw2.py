9d6d714024a02cb748cb9bfb98f48837561a36ec
Ари <arianna2811@yandex.ru>
2025-07-20 13:45:18 +0300

    feat: class Vector, init and get x,y,z

---
+++ hw2.py
@@ -0,0 +1,15 @@
+class Vector:
+
+    def __init__(self, x, y, z):
+        self.__x = x
+        self.__y = y
+        self.__z = z
+
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
+
+    def get_z(self):
+        return self.__z
