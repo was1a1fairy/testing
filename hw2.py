2cdb61d3a5ec1b4010fbab2600683412910dbff3
Ари <arianna2811@yandex.ru>
2025-07-21 13:13:04 +0300

    feat: add random_vector and str

--- hw2.py
+++ hw2.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import random
 class Vector:
 
     def __init__(self, x, y, z):
@@ -30,3 +31,8 @@ class Vector:
     def angle_between(self, other: Vector):
         return (self.scalar_mul(other))/(((self.__z**2 + self.__y**2 + self.__x**2)**0.5) * (other.get_y()**2 + other.get_x()**2 + other.get_z()**2)**0.5)
 
+    def random_vector(self):
+        return Vector(random.randint(1,10), random.randint(1,10), random.randint(1,10))
+
+    def __str__(self):
+        return f"Vector: {self.__x}, {self.__y}, {self.__z}."
