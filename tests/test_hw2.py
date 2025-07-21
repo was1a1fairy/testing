7f7b101f536d97b6f6bdbe0cef9ff11498f64e37
Ари <arianna2811@yandex.ru>
2025-07-21 17:14:38 +0300

    feat: add positive tests

--- tests/test_hw2.py
+++ tests/test_hw2.py
@@ -2,6 +2,21 @@ from hw2 import Vector
 import pytest
 
 @pytest.fixture()
-def vector():
-    return Vector.random_vector()
+def vec1():
+    return Vector(2,3,5)
 
+@pytest.fixture()
+def vec2():
+    return Vector(1, -2, 4)
+
+def test_random_vector(vec1):
+    assert vec1.random_vector()#what how
+
+def test_lenth(vec1):
+    assert vec1.lenth() == 38**0.5
+
+def test_scalar_mul(vec1, vec2):
+    assert vec1.scalar_mul(vec2) == 16
+
+def test_angle_between(vec1, vec2):
+    assert vec1.angle_between(vec2) == 16 / ((38 ** 0.5) * (21 ** 0.5))
